cf895fc05155c816b5fd76ba567d212a4ff6d97a
Andrew Russell <andy.russell.code@gmail.com>
2017-04-01 11:16:06 -0500

    added post func and assigned self.requests to 4 variables

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
 import cgi
+import re
 
 #html biolerplate for top of page
 header = """
@@ -32,33 +17,56 @@ header = """
 <body>
     <h1>User Signup</h1>
 """
-
-generic_form = """
-<form action='add-user' method='post'>
-    <label>Username: <input type='text' name='username'/></label>
-    <br>
-    <label>Password: <input type='text' name='password'/></label>
-    <br>
-    <label>Confrim Password: <input type='text' name='conf-password'/></label>
-    <br>
-    <label>Email (optional): <input type='text' name='email'/></label>
-    <br>
-    <input type="submit" value="Submit"/>
-</form>
-"""
+def generic_form(username, password, verify, email):
+    form = """
+    <form action='/' method='post'>
+        <label>Username: <input type='text' name='username' value='{0}'/></label>
+        <br>
+        <label>Password: <input type='text' name='password' value='{1}'/></label>
+        <br>
+        <label>Confrim Password: <input type='text' name='verify' value='{2}'/></label>
+        <br>
+        <label>Email (optional): <input type='text' name='email' value='{3}'/></label>
+        <br>
+        <input type="submit" value="Submit"/>
+    </form>
+    """.format(username, password, verify, email)
+    return(form)
 
 #html biolerplate for bottom of page
 footer = """
 </body>
 </html>
 """
+#regular expressions for verifying inputs
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+PASSWORD_RE = (r"^.{3,20}$")
+EMAIL_RE =  (r"^[\S]+@[\S]+.[\S]+$")
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        """Handles requeststs to main (/) page"""
-        main_content = generic_form
+        """Handles requests to main (/) page"""
+        username = ""
+        password = ""
+        verify = ""
+        email = ""
+        main_content = generic_form(username, password, verify, email)
         content = (header + main_content + footer)
         self.response.write(content) #print the content to the page
 
+    def post(self):
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+
+
+
+class Welcome(webapp2.RequestHandler):
+    """Merely prints welcome message"""
+    #def post(self):
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/welcome', Welcome)
 ], debug=True)
